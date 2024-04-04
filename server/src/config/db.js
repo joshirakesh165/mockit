724136d72ef9f8173abec850fd67137219097976
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 17:13:38 +0530

    Code pushed from Api

---
+++ server/src/config/db.js
@@ -0,0 +1,20 @@
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const url = process.env.MONGO_DB_URL
+
+// const createDBConnection = async () => {
+//     try {
+//         const conn = await mongoose.connect(url, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch(error) {
+//         console.log(`MongoDB  not Connected: ${url}`);
+//         process.exit(1);
+//     }
+// }
+
+// export default createDBConnection;
