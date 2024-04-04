936d828b32512d7cd21bcf5c9640aa5425f0b554
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 15:17:39 +0530

    Code pushed from Api

---
+++ server/src/mocker/data/default-data/ecom/carts.js
@@ -0,0 +1,193 @@
+const carts = [
+     {
+        "id": 'CART-001',
+        "userId": 'USER-001',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-002',
+        "userId": 'USER-002',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-003',
+        "userId": 'USER-003',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-004',
+        "userId": 'USER-004',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-005',
+        "userId": 'USER-005',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-006',
+        "userId": 'USER-006',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-007',
+        "userId": 'USER-007',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-008',
+        "userId": 'USER-008',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-009',
+        "userId": 'USER-009',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    },
+    {
+        "id": 'CART-010',
+        "userId": 'USER-010',
+        "date": 1352572200000,
+        "products": [
+            {
+                productId: 'PROD-001',
+                quantity: 4
+            },
+            {
+                productId: 'PROD-002',
+                quantity: 10
+            },
+            {
+                productId: 'PROD-003',
+                quantity: 2
+            }
+        ]
+    }
+]
+export default carts;
