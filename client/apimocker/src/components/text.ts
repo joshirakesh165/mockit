6d4a0efb9539bce4a701c0d65849c4d5d7f6ac17
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 15:17:12 +0530

    Code pushed from Api

---
+++ client/apimocker/src/components/text.ts
@@ -0,0 +1,15 @@
+const text = {
+    TRY_IT : 'Try it',
+    MESSAGE: 'Run this code here, in a console or copy url and hit enter in browser',
+    RUN:'Show output',
+    HIDE:'Hide output',
+    GET_ALL: 'Fetch all ',
+    GET_SINGLE :'Fetch Single {item} by id',
+    LIMIT :'Limit results',
+    GET_BY_FIELD:'Fetch {item} by {field}',
+    ADD: 'Add new {item}',
+    UPDATE:'Update {item}',
+    DELETE_ALL: 'Delete a {item}' 
+
+}
+export default text;
