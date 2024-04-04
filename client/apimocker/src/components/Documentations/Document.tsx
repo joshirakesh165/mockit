a0a7dfb5c2318a965b8d4e4aeb8f389fca3c56e7
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 17:13:14 +0530

    Code pushed from Api

---
+++ client/apimocker/src/components/Documentations/Document.tsx
@@ -0,0 +1,23 @@
+
+import Doc from "../Doc"
+const baseURL = 'https://apimocker.onrender.com/api/v1'
+// const baseURL = 'http://localhost:8000/default/'
+
+const Document = (props: any) => {
+  let catagories = Object.keys(props.apiInfo)
+
+  const DocUI = (api:any) => <Doc key={api.title} apiInfo={{ body: api.body, url: baseURL + api.url, method: api.method, description: api.description, response: api.response, title: api.title }} />
+
+  return (
+    <>
+      {catagories.map(category => (
+        Object.keys(props.apiInfo[category]).map(name => (<div key={name}>
+          <h2>{name}</h2>
+          {props.apiInfo[category][name].map(api => DocUI(api))}
+        </div>))
+      ))}
+    </>
+  )
+}
+
+export default Document
