3b8833e1278bfa137893ab955ba1bc917f197d03
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 17:01:32 +0530

    Code pushed from Api

---
+++ server/src/mocker/data/default-data/ecom/user.js
@@ -0,0 +1,223 @@
+const users = [
+    {
+        id: "USER-001",
+        email:'andrew@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-002",
+        email:'tot@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-003",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-004",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-005",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-006",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-007",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-008",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-009",
+        email:'John@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    },
+    {
+        id: "USER-010",
+        email:'nohn@gmail.com',
+        username:'johnd',
+        password:'TEST$#123',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            line1:'Apt 101',
+            line2:'123 Main Street',
+            city:'Cityville',
+            state:'CA',
+            zipcode:'12345',
+            geolocation:{
+                lat:'-31.1159',
+                long:'41.1496'
+            }
+        },
+        phone:'91234567890'
+    }
+]
+export default users;
