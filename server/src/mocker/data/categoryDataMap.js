cc172b1911d360f2ebb272859a08d1ede2e62702
joshirakesh165 <joshirakesh165@gmail.com>
2024-04-04 17:01:25 +0530

    Code pushed from Api

---
+++ server/src/mocker/data/categoryDataMap.js
@@ -0,0 +1,15 @@
+import animal from "./animal.js";
+import user from "./user.js";
+import firstName from "./firstName.js";
+import lastName from "./lastName.js";
+
+let categoryMap = {
+    animal,
+    user,
+    name:user,
+    fullName:user,
+    firstName,
+    lastName
+
+}
+export default categoryMap;
